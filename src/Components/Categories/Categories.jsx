7c4cc86a5ea5364fc96155bcd5f76f25f2819474
unknown <amiramamdouh320@gmail.com>
2024-07-07 00:00:42 +0300

    Update

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -1,55 +1,50 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
 import { Oval } from "react-loader-spinner";
+import { useQuery } from "react-query";
 
 export default function Categories() {
-  const [allCategories, setAllCategories] = useState(null);
-
   async function getAllCategories() {
-    const { data } = await axios.get(
-      `https://ecommerce.routemisr.com/api/v1/categories`
-    );
-    setAllCategories(data.data);
+    return await axios.get(`https://ecommerce.routemisr.com/api/v1/categories`);
   }
 
-  useEffect(() => {
-    getAllCategories();
-  }, []);
+  const { data, isLoading } = useQuery("getAllCategories", getAllCategories);
+
+  if (isLoading) {
+    return (
+      <div className="d-flex vh-100 justify-content-center align-items-center opacity-100 bg-main-light ">
+        <Oval
+          visible={true}
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="oval-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+    );
+  }
 
   return (
     <>
-      {allCategories ? (
-        <div className="container-fluid py-5">
-          <div className="row g-5">
-            {allCategories.map((category, index) => (
-              <div key={index} className="col-6 col-md-3">
-                <div className="category">
-                  <div className="text-center mb-2">
-                    <h3 className="py-2">{category.name}</h3>
-                  </div>
-                  <img
-                    className="w-100"
-                    src={category.image}
-                    alt={category.slug}
-                  />
+      <div className="container-fluid py-5">
+        <div className="row g-5">
+          {data.data.data.map((category, index) => (
+            <div key={index} className="col-6 col-md-3">
+              <div className="category">
+                <div className="text-center mb-2">
+                  <h3 className="py-2">{category.name}</h3>
                 </div>
+                <img
+                  className="w-100"
+                  src={category.image}
+                  alt={category.slug}
+                />
               </div>
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
-      ) : (
-        <div className="d-flex vh-100 justify-content-center align-items-center opacity-100 bg-main-light ">
-          <Oval
-            visible={true}
-            height="80"
-            width="80"
-            color="#4fa94d"
-            ariaLabel="oval-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-          />
-        </div>
-      )}
+      </div>
     </>
   );
 }
