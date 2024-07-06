a1cdb6d583337d8d4fcd1e47b22cb2fbde6d2000
unknown <amiramamdouh320@gmail.com>
2024-07-06 08:25:00 +0300

    Update

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -14,7 +14,7 @@ export default function Products() {
     setAllProducts(data.data);
   }
 
-  useQuery();
+  useQuery("getAllProducts", getAllProducts);
 
   useEffect(() => {
     getAllProducts();
