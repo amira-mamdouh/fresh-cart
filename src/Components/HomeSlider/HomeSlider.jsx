7d31b96120c76ccdeecdf662880f752a60baa520
unknown <amiramamdouh320@gmail.com>
2024-07-08 15:03:22 +0300

    Update

--- src/Components/HomeSlider/HomeSlider.jsx
+++ src/Components/HomeSlider/HomeSlider.jsx
@@ -2,6 +2,14 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+// Import images directly
+import sliderImage1 from "../../assets/images/slider-image-1.jpeg";
+import sliderImage2 from "../../assets/images/slider-image-2.jpeg";
+import sliderImage3 from "../../assets/images/slider-image-3.jpeg";
+import sliderImage4 from "../../assets/images/slider-2.jpeg";
+import groceryBanner1 from "../../assets/images/grocery-banner.png";
+import groceryBanner2 from "../../assets/images/grocery-banner-2.jpeg";
+
 export default function SimpleSlider() {
   var settings = {
     dots: true,
@@ -13,25 +21,22 @@ export default function SimpleSlider() {
   return (
     <Slider {...settings}>
       <div>
-        <img src={require("../../assets/images/slider-image-1.jpeg")} alt="" />
+        <img className="w-100" src={sliderImage1} alt="Slider Img" />
       </div>
       <div>
-        <img src={require("../../assets/images/slider-image-2.jpeg")} alt="" />
+        <img className="w-100" src={sliderImage2} alt="Slider Img" />
       </div>
       <div>
-        <img src={require("../../assets/images/slider-image-3.jpeg")} alt="" />
+        <img className="w-100" src={sliderImage3} alt="Slider Img" />
       </div>
       <div>
-        <img src={require("../../assets/images/slider-2.jpeg")} alt="" />
+        <img className="w-100" src={sliderImage4} alt="Slider Img" />
       </div>
       <div>
-        <img src={require("../../assets/images/grocery-banner.png")} alt="" />
+        <img className="w-100" src={groceryBanner1} alt="Slider Img" />
       </div>
       <div>
-        <img
-          src={require("../../assets/images/grocery-banner-2.jpeg")}
-          alt=""
-        />
+        <img className="w-100" src={groceryBanner2} alt="Slider Img" />
       </div>
     </Slider>
   );
