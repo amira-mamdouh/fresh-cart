fbe25a230751b7c5bbcad59a736c999626fa8196
unknown <amiramamdouh320@gmail.com>
2024-07-09 18:11:57 +0300

    Update

--- src/Components/CategoriesSlider/CategoriesSlider.jsx
+++ src/Components/CategoriesSlider/CategoriesSlider.jsx
@@ -1,6 +1,8 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 export default function CategoriesSlider() {
   var settings = {
@@ -10,14 +12,18 @@ export default function CategoriesSlider() {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+
+  function getAllCategories() {
+    return axios.get(`https://ecommerce.routemisr.com/api/v1/categories`);
+  }
+
+  const { data } = useQuery("getAllCategories", getAllCategories);
+
   return (
     <div className="row">
       <div className="col-md-9">
         <Slider {...settings}>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          {data.data.data.map((category, index) => {})}
         </Slider>
       </div>
     </div>
