f6c8ccffbb056bf36191c765fa77a007a0dcfc59
unknown <amiramamdouh320@gmail.com>
2024-07-11 21:46:41 +0300

    Update

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -4,8 +4,11 @@ import { createContext } from "react";
 export const cartContext = createContext();
 
 export default function CartContextProvider({ children }) {
-  function addProductToCart() {
-    axios.post();
+  function addProductToCart(id) {
+    axios.post(`https://ecommerce.routemisr.com/api/v1/cart`, {
+      productId: id, {
+        headers:{token: localStorage.getItem('token')}
+    }).then();
   }
 
   return <cartContext.Provider>{children}</cartContext.Provider>;
