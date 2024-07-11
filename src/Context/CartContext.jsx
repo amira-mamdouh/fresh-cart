78d4902daf21bd794833e6c493b82b7fbe6bd74e
unknown <amiramamdouh320@gmail.com>
2024-07-11 21:39:04 +0300

    Update

--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,3 +1,12 @@
-export default function CartContextProvider() {
-  return <div>CartContext</div>;
+import axios from "axios";
+import { createContext } from "react";
+
+export const cartContext = createContext();
+
+export default function CartContextProvider({ children }) {
+ function addProductToCart() {
+  axios.post()
+ }
+ 
+  return <cartContext.Provider value={}>{children}</cartContext.Provider>;
 }
