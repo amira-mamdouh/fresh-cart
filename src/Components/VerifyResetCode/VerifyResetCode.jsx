8fb43de74db393d306423c59a131ce1a76cf746b
unknown <amiramamdouh320@gmail.com>
2024-07-17 23:16:37 +0300

    Update

--- src/Components/VerifyResetCode/VerifyResetCode.jsx
+++ src/Components/VerifyResetCode/VerifyResetCode.jsx
@@ -1,101 +1,101 @@
-import axios from "axios";
-import { useFormik } from "formik";
-import { useState } from "react";
-import { ColorRing } from "react-loader-spinner";
-import { useNavigate } from "react-router-dom";
-import * as yup from "yup";
+// import axios from "axios";
+// import { useFormik } from "formik";
+// import { useState } from "react";
+// import { ColorRing } from "react-loader-spinner";
+// import { useNavigate } from "react-router-dom";
+// import * as yup from "yup";
 
-// Validation schema using Yup
-const mySchema = yup.object({
-  resetCode: yup.string().required("Please enter your reset code."),
-});
+// // Validation schema using Yup
+// const mySchema = yup.object({
+//   resetCode: yup.string().required("Please enter your reset code."),
+// });
 
-export default function VerifyResetCode() {
-  const [isUnSuccess, setIsUnSuccess] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [message, setMessage] = useState("");
-  const navigate = useNavigate();
+// export default function VerifyResetCode() {
+//   const [isUnSuccess, setIsUnSuccess] = useState(false);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [message, setMessage] = useState("");
+//   const navigate = useNavigate();
 
-  const myFormik = useFormik({
-    initialValues: {
-      resetCode: "",
-    },
-    validationSchema: mySchema,
-    onSubmit: async (values) => {
-      setIsLoading(true);
-      setIsUnSuccess(false);
-      try {
-        await axios.post(
-          `https://ecommerce.routemisr.com/api/v1/auth/verifyResetCode`,
-          values
-        );
-        navigate("/updateData", {
-          state: { resetCode: values.resetCode },
-        });
-      } catch (error) {
-        if (error.response) {
-          setMessage(error.response.data.message);
-        } else {
-          setMessage("An unexpected error occurred.");
-        }
-        setIsUnSuccess(true);
-        setTimeout(() => {
-          setIsUnSuccess(false);
-        }, 3000);
-      } finally {
-        setIsLoading(false);
-      }
-    },
-  });
+//   const myFormik = useFormik({
+//     initialValues: {
+//       resetCode: "",
+//     },
+//     validationSchema: mySchema,
+//     onSubmit: async (values) => {
+//       setIsLoading(true);
+//       setIsUnSuccess(false);
+//       try {
+//         await axios.post(
+//           `https://ecommerce.routemisr.com/api/v1/auth/verifyResetCode`,
+//           values
+//         );
+//         navigate("/updateData", {
+//           state: { resetCode: values.resetCode },
+//         });
+//       } catch (error) {
+//         if (error.response) {
+//           setMessage(error.response.data.message);
+//         } else {
+//           setMessage("An unexpected error occurred.");
+//         }
+//         setIsUnSuccess(true);
+//         setTimeout(() => {
+//           setIsUnSuccess(false);
+//         }, 3000);
+//       } finally {
+//         setIsLoading(false);
+//       }
+//     },
+//   });
 
-  return (
-    <div className="container-sm w-md-75 m-md-auto p-5">
-      {isUnSuccess && (
-        <div className="alert alert-danger text-center">{message}</div>
-      )}
-      <h3 className="mb-3">Verify Reset Code:</h3>
-      <form onSubmit={myFormik.handleSubmit}>
-        <label htmlFor="resetCode">Reset Code:</label>
-        <input
-          value={myFormik.values.resetCode}
-          onChange={myFormik.handleChange}
-          onBlur={myFormik.handleBlur}
-          type="text"
-          id="resetCode"
-          name="resetCode"
-          className={`form-control mb-2 ${
-            myFormik.errors.resetCode && myFormik.touched.resetCode
-              ? "is-invalid"
-              : ""
-          }`}
-        />
-        {myFormik.touched.resetCode && myFormik.errors.resetCode && (
-          <div className="invalid-feedback mb-3">
-            {myFormik.errors.resetCode}
-          </div>
-        )}
-        <div className="button text-end">
-          <button
-            className="btn bg-main text-white"
-            type="submit"
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <ColorRing
-                visible={true}
-                height="35"
-                width="35"
-                ariaLabel="color-ring-loading"
-                wrapperStyle={{}}
-                wrapperClass="color-ring-wrapper"
-                colors={["#fff", "#fff", "#fff", "#fff", "#fff"]}
-              />
-            ) : (
-              "Verify"
-            )}
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-}
+//   return (
+//     <div className="container-sm w-md-75 m-md-auto p-5">
+//       {isUnSuccess && (
+//         <div className="alert alert-danger text-center">{message}</div>
+//       )}
+//       <h3 className="mb-3">Verify Reset Code:</h3>
+//       <form onSubmit={myFormik.handleSubmit}>
+//         <label htmlFor="resetCode">Reset Code:</label>
+//         <input
+//           value={myFormik.values.resetCode}
+//           onChange={myFormik.handleChange}
+//           onBlur={myFormik.handleBlur}
+//           type="text"
+//           id="resetCode"
+//           name="resetCode"
+//           className={`form-control mb-2 ${
+//             myFormik.errors.resetCode && myFormik.touched.resetCode
+//               ? "is-invalid"
+//               : ""
+//           }`}
+//         />
+//         {myFormik.touched.resetCode && myFormik.errors.resetCode && (
+//           <div className="invalid-feedback mb-3">
+//             {myFormik.errors.resetCode}
+//           </div>
+//         )}
+//         <div className="button text-end">
+//           <button
+//             className="btn bg-main text-white"
+//             type="submit"
+//             disabled={isLoading}
+//           >
+//             {isLoading ? (
+//               <ColorRing
+//                 visible={true}
+//                 height="35"
+//                 width="35"
+//                 ariaLabel="color-ring-loading"
+//                 wrapperStyle={{}}
+//                 wrapperClass="color-ring-wrapper"
+//                 colors={["#fff", "#fff", "#fff", "#fff", "#fff"]}
+//               />
+//             ) : (
+//               "Verify"
+//             )}
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -1,64 +1,64 @@
-// import axios from "axios";
-// import { createContext, useContext, useEffect, useState } from "react";
-// import { authContext } from "./AuthContext";
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+import { authContext } from "./AuthContext";
 
-// export const cartContext = createContext();
+export const cartContext = createContext();
 
-// export default function CartContextProvider({ children }) {
-//   const { token } = useContext(authContext);
+export default function CartContextProvider({ children }) {
+  const { token } = useContext(authContext);
 
-//   const [numOfCartItems, setNumOfCartItems] = useState(0);
-//   const [totalCartPrice, setTotalCartPrice] = useState(0);
-//   const [allProducts, setAllProducts] = useState(null);
+  const [numOfCartItems, setNumOfCartItems] = useState(0);
+  const [totalCartPrice, setTotalCartPrice] = useState(0);
+  const [allProducts, setAllProducts] = useState(null);
 
-//   function addProductToCart(id) {
-//     return axios
-//       .post(
-//         `https://ecommerce.routemisr.com/api/v1/cart`,
-//         { productId: id },
-//         { headers: { token: localStorage.getItem("token") } }
-//       )
-//       .then((response) => {
-//         getUserCart(); // Update the cart after adding a product
-//         return response.data;
-//       })
-//       .catch((error) => {
-//         console.error("Error adding product to cart:", error);
-//         throw error;
-//       });
-//   }
+  function addProductToCart(id) {
+    return axios
+      .post(
+        `https://ecommerce.routemisr.com/api/v1/cart`,
+        { productId: id },
+        { headers: { token: localStorage.getItem("token") } }
+      )
+      .then((response) => {
+        getUserCart(); // Update the cart after adding a product
+        return response.data;
+      })
+      .catch((error) => {
+        console.error("Error adding product to cart:", error);
+        throw error;
+      });
+  }
 
-//   function getUserCart() {
-//     axios
-//       .get(`https://ecommerce.routemisr.com/api/v1/cart`, {
-//         headers: { token: localStorage.getItem("token") },
-//       })
-//       .then((res) => {
-//         setNumOfCartItems(res.data.numOfCartItems);
-//         setTotalCartPrice(res.data.data.totalCartPrice);
-//         setAllProducts(res.data.data.products);
-//       })
-//       .catch((err) => {
-//         console.error("Error fetching user cart:", err);
-//       });
-//   }
+  function getUserCart() {
+    axios
+      .get(`https://ecommerce.routemisr.com/api/v1/cart`, {
+        headers: { token: localStorage.getItem("token") },
+      })
+      .then((res) => {
+        setNumOfCartItems(res.data.numOfCartItems);
+        setTotalCartPrice(res.data.data.totalCartPrice);
+        setAllProducts(res.data.data.products);
+      })
+      .catch((err) => {
+        console.error("Error fetching user cart:", err);
+      });
+  }
 
-//   useEffect(() => {
-//     if (token) {
-//       getUserCart();
-//     }
-//   }, [token]);
+  useEffect(() => {
+    if (token) {
+      getUserCart();
+    }
+  }, [token]);
 
-//   return (
-//     <cartContext.Provider
-//       value={{
-//         addProductToCart,
-//         numOfCartItems,
-//         totalCartPrice,
-//         allProducts,
-//       }}
-//     >
-//       {children}
-//     </cartContext.Provider>
-//   );
-// }
+  return (
+    <cartContext.Provider
+      value={{
+        addProductToCart,
+        numOfCartItems,
+        totalCartPrice,
+        allProducts,
+      }}
+    >
+      {children}
+    </cartContext.Provider>
+  );
+}
