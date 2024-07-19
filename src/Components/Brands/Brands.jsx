a0223b7cb5b454000b5f396aba2712812127b6df
unknown <amiramamdouh320@gmail.com>
2024-07-19 19:57:14 +0300

    Update

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,29 +1,8 @@
 import axios from "axios";
 import { useQuery } from "react-query";
-import SimpleSlider from "../HomeSlider/HomeSlider";
-import CategoriesSlider from "../CategoriesSlider/CategoriesSlider";
-import { Link } from "react-router-dom";
 import Loading from "../Loading/Loading";
-import toast from "react-hot-toast";
 
-// The Brands component fetches and displays a list of Brands.
 export default function Brands() {
-  // Function to handle adding a product to the cart.
-  // async function addProduct(id) {
-  //   const res = await addProductToCart(id);
-  //   if (res.status === "success") {
-  //     toast.success("Product added to cart successfully", {
-  //       duration: 2000,
-  //       position: "bottom-right",
-  //     });
-  //   } else {
-  //     toast.error("Faild to add", {
-  //       duration: 2000,
-  //       position: "bottom-right",
-  //     });
-  //   }
-  // }
-
   // Function to fetch all Brands from the API.
   async function getAllBrands() {
     return await axios.get(`https://ecommerce.routemisr.com/api/v1/brands`);
